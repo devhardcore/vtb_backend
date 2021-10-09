6cac7d2a9d2529931cd6e3f9be57ab4011b89a17
Денис Токарев <yan.netsky@mail.ru>
2021-10-10 00:38:59 +0600

    [+]

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ db_level = deta.Base("levels")
 
 db_links = deta.Base("links")
 
-
+db_users_stat = deta.Base("users_stat")
 
 
 @app.post("/create_new_user")
@@ -52,14 +52,82 @@ async def insert_coins(n: int, token: str):
     db_coins.update(update_module, key)
 
 
-@app.post("/levels")
-async def levels(token: str):
-    test_fetch = db_coins.fetch({"Token": token})
+@app.post("/count_levels")
+async def count_levels(token: str):
+    test_fetch = db_level.fetch({"Token": token})
     key = test_fetch.items[0]["key"]
     update_module = {
-        "levels": test_fetch.items[0]["Coins"] + 1
+        "Level": test_fetch.items[0]["Level"] + 1
     }
+    db_level.update(update_module, key)
 
 
+@app.post("/level_info")
+async def level_info(lvl_id: int, par: str, token: str):
+    test_fetch = db_users_stat.fetch({"Token": token})
+    key = test_fetch.items[0]["key"]
+    update_module = {}
+    if lvl_id == 1 and not test_fetch.items[0]["First_lvl"]:
+        update_module = {
+            "First_lvl": True,
+            "First_par": par
+        }
+    elif lvl_id == 2 and not test_fetch.items[0]["Second_lvl"]:
+        update_module = {
+            "Second_lvl": True,
+            "Second_par": par
+        }
+    elif lvl_id == 3 and not test_fetch.items[0]["Third_lvl"]:
+        update_module = {
+            "Third_lvl": True,
+            "Third_par": par
+        }
+
+    db_users_stat.update(update_module, key)
+
+
+@app.post("/is_vtb")
+async def post_vtb_info(token: str):
+    test_fetch = db_users_stat.fetch({"Token": token})
+    key = test_fetch.items[0]["key"]
+
+    update_module = {}
+
+    if not test_fetch.items[0]["is_vtb"]:
+        update_module = {
+            "is_vtb": True
+        }
+
+    db_users_stat.update(update_module, key)
+
+
+@app.post("/is_invest")
+async def post_invest_info(token: str):
+    test_fetch = db_users_stat.fetch({"Token": token})
+    key = test_fetch.items[0]["key"]
+
+    update_module = {}
+
+    if not test_fetch.items[0]["is_invest"]:
+        update_module = {
+            "is_invest": True
+        }
+
+    db_users_stat.update(update_module, key)
+
+
+@app.post("/is_social")
+async def post_social_info(token: str):
+    test_fetch = db_users_stat.fetch({"Token": token})
+    key = test_fetch.items[0]["key"]
+
+    update_module = {}
+
+    if not test_fetch.items[0]["is_social"]:
+        update_module = {
+            "is_social": True
+        }
+
+    db_users_stat.update(update_module, key)
 
 
