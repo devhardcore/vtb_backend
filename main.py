ad7f66f66d2d63da0c8bf7ee8cf535af17918600
Денис Токарев <yan.netsky@mail.ru>
2021-10-09 21:21:07 +0600

    [+]Added Auth and edit coins functions

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from fastapi import FastAPI
-# import sqlalchemy
+from deta import Deta
+
 
 app = FastAPI()
 
-themes = {1, 2, 3}
+deta = Deta('c0jrkegd_rQMSFSXNmgb14J4dBL7hGzbrZq2YV12f')
+
+db_users = deta.Base("users")
+
+db_coins = deta.Base("coins")
 
 
-@app.get("/themes")
-def get_list_of_themes():
-    return {"themes": themes}
+@app.post("/create_new_user")
+async def create_new_user(username: str, token: str, segment: int):
+    db_users.put({
+        "Username": username,
+        "Token": token,
+        "Segment": segment
+    })
 
 
-@app.get("/theme/{theme_id}")
-def get_theme(theme_id: int):
-    return{
-        "theme_id": theme_id,
-        "name": "...",
-        "theory": "...",
-        "links": "...",
-        "quiz": [{
-            "question": "...",
-            "image_url": "...",
-            "wrong_answers": "...",
-            "answer": "...",
-            "points": "..."
-        }],
+@app.post("/insert_coins")
+async def insert_coins(n, token):
+    test_fetch = db_coins.fetch({"Token": token})
+    key = test_fetch.items[0]["key"]
+    update_module = {
+        "Coins": test_fetch.items[0]["Coins"] + n
     }
+    db_coins.update(update_module, key)
