301849eb33bb091c8f6052245ad291e91ae6084b
Денис Токарев <yan.netsky@mail.ru>
2021-10-09 23:05:46 +0600

    [+] level endpoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from deta import Deta
+# from config import config
 
 
 app = FastAPI()
@@ -10,6 +11,12 @@ db_users = deta.Base("users")
 
 db_coins = deta.Base("coins")
 
+db_level = deta.Base("levels")
+
+db_links = deta.Base("links")
+
+
+
 
 @app.post("/create_new_user")
 async def create_new_user(username: str, token: str, segment: int):
@@ -19,12 +26,40 @@ async def create_new_user(username: str, token: str, segment: int):
         "Segment": segment
     })
 
+    db_coins.put({
+        "Coins": 0,
+        "Token": token
+    })
+
+    db_level.put({
+        "levels": 0,
+        "Token": token
+    })
+
+    db_coins.put({
+        "Token": token,
+        "link": False
+    })
+
 
 @app.post("/insert_coins")
-async def insert_coins(n, token):
+async def insert_coins(n: int, token: str):
     test_fetch = db_coins.fetch({"Token": token})
     key = test_fetch.items[0]["key"]
     update_module = {
         "Coins": test_fetch.items[0]["Coins"] + n
     }
     db_coins.update(update_module, key)
+
+
+@app.post("/levels")
+async def levels(token: str):
+    test_fetch = db_coins.fetch({"Token": token})
+    key = test_fetch.items[0]["key"]
+    update_module = {
+        "levels": test_fetch.items[0]["Coins"] + 1
+    }
+
+
+
+
